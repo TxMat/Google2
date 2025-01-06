5defbab7dd265e9c596f2667af24843bbdbd3dd6
Mathieu Ponal <mathieu.ponal@gmail.com>
2025-01-06 23:34:30 +0100

    simplify

--- main.py
+++ main.py
@@ -1,67 +1,46 @@
 import time
-from typing import Dict, Any, Tuple
-
+import logging
+from typing import Dict
 import praw
 import urllib3
 import xmltodict
 import pandas as pd
 from praw.models import Submission
-
 from Author import Author
 from Corpus import Corpus
 from Document import Document
 from SearchEngine import SearchEngine
-
-import coloredlogs, logging
+import coloredlogs
 
 id2doc = {}
-id2aut: dict[str, Author] = {}
+id2aut: Dict[str, Author] = {}
 
 logger = logging.getLogger(__name__)
 
-def reddit_import(subject : str, nb_doc: int):
-    reddit_client = praw.Reddit(client_id='T9qPLZ2H3esl_ukXzn0UVA', client_secret='zyGz9sEs67D0LB3Ihu3kia_3oq0KlQ',
-                                user_agent='search_engine_td')
-    key = 0
-    submission: Submission
-    # todo sbr title should be a param
-    for submission in reddit_client.subreddit(subject).hot(limit=nb_doc):
-        if submission.author and submission.author.name not in id2aut:
-            id2aut[submission.author.name] = Author(submission.author.name)
+def reddit_import(subject: str, nb_doc: int):
+    reddit_client = praw.Reddit(client_id='T9qPLZ2H3esl_ukXzn0UVA', client_secret='zyGz9sEs67D0LB3Ihu3kia_3oq0KlQ', user_agent='search_engine_td')
+    for key, submission in enumerate(reddit_client.subreddit(subject).hot(limit=nb_doc)):
+        author_name = submission.author.name if submission.author else "Unknown"
+        if author_name not in id2aut:
+            id2aut[author_name] = Author(author_name)
         date = pd.to_datetime(int(submission.created_utc), utc=True, unit='s')
-        doc: Document = Document(submission.title, id2aut[submission.author.name], date, submission.url, submission.selftext, "reddit")
+        doc = Document(submission.title, id2aut[author_name], date, submission.url, submission.selftext, "reddit")
         id2doc[f"red{key}"] = doc
-        id2aut[submission.author.name].add_document(doc)
-        key+=1
-
+        id2aut[author_name].add_document(doc)
 
 def arxiv_import(subject: str, nb_doc: int):
     resp = urllib3.request('GET', f'http://export.arxiv.org/api/query?search_query=all:{subject}&max_results={nb_doc}')
-    generated_dict = xmltodict.parse(resp.data)
-
-    key = 0
-
-    for articles in generated_dict['feed']['entry']:
-        title = articles['title'].replace("\n", " ")
-        txt = articles['summary'].replace("\n", " ")
-        if len(articles['author']) == 1:
-            a = articles['author']["name"]
-        elif len(articles['author']) > 1:
-            if type(articles['author']) is list:
-                a = articles['author'][0]['name']
-            else:
-                a = "Cannot parse"
-        else:
-            a = "Unknown"
-
-        if a not in id2aut:
-            id2aut[a] = Author(a)
-
-        date = pd.to_datetime(articles['published'])
-        doc = Document(title, id2aut[a], date, articles['id'], txt, "arxiv")
+    articles = xmltodict.parse(resp.data)['feed']['entry']
+    for key, article in enumerate(articles):
+        title = article['title'].replace("\n", " ")
+        summary = article['summary'].replace("\n", " ")
+        author_name = article['author'][0]['name'] if isinstance(article['author'], list) else article['author']['name']
+        if author_name not in id2aut:
+            id2aut[author_name] = Author(author_name)
+        date = pd.to_datetime(article['published'])
+        doc = Document(title, id2aut[author_name], date, article['id'], summary, "arxiv")
         id2doc[f"arx{key}"] = doc
-        id2aut[a].add_document(doc)
-        key+=1
+        id2aut[author_name].add_document(doc)
 
 def us_speeches_import():
     us_data = pd.read_csv("data/discours_US.csv", sep="\t")
@@ -83,7 +62,7 @@ def us_speeches_import():
             counter += 1
 
 
-def build_corpus(subject : str, nb: int) -> Corpus:
+def build_corpus(subject: str, nb: int) -> Corpus:
     corpus = Corpus("Main Corpus")
     try:
         reddit_import(subject, nb)
@@ -94,43 +73,35 @@ def build_corpus(subject : str, nb: int) -> Corpus:
     except Exception as e:
         print(f"Error while importing arxiv data: {e}")
     us_speeches_import()
-
-    d: Document
-    for d in id2doc.values():
-        corpus.add(d)
-
+    for doc in id2doc.values():
+        corpus.add(doc)
     return corpus
 
 def get_search_engine(corpus: Corpus) -> SearchEngine:
     return SearchEngine(corpus)
 
-
-def init(subject: str, nb : int, should_build_corpus: bool) -> SearchEngine:
+def init(subject: str, nb: int, should_build_corpus: bool) -> SearchEngine:
     coloredlogs.install(level='DEBUG')
     start_time = time.time()
-    if should_build_corpus :
+    if should_build_corpus:
         logger.info("Building corpus")
         corpus = build_corpus(subject, nb)
         save_corpus(corpus)
     else:
         logger.warning("should_build_corpus is set to False, loading corpus from file")
         corpus = load_corpus()
-
     logger.info(f"Corpus built in {round(time.time() - start_time, 2)} seconds")
     logger.info("Corpus stats:")
     print(corpus.stats())
     return get_search_engine(corpus)
 
-
 def save_corpus(corpus: Corpus):
     import pickle
-
     with open("corpus.pkl", "wb") as f:
         pickle.dump(corpus, f)
 
 def load_corpus() -> Corpus:
     import pickle
-
     with open("corpus.pkl", "rb") as f:
         return pickle.load(f)
 
